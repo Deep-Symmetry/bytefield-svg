0c50ef7f091257ccfb0a7ad584462ccf5966d37e
James Elliott <james@deepsymmetry.org>
2020-03-23 22:57:07 -0500

    Set up for building user guide Antora site.

---
+++ doc/build.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# This script is run by Netlify continuous integration to build the
+# Antora site hosting the bytefield-svg user guide.
+
+npm install yarn
+npm install asciidoctor-mathjax
+npm install bytefield-svg
+cd doc
+
+# Build the unreleased branch of Antora that supports plugins
+if [ ! -d "antora" ]
+then
+    git clone https://gitlab.com/djencks/antora.git
+    cd antora
+    git checkout issue-585-with-377-582-git-credential-plugin
+    ../../node_modules/.bin/yarn
+    cd ..
+fi
+
+# Build the unreleased Antora LUNR plugin
+if [ ! -d "antora-lunr" ]
+then
+    git clone https://github.com/djencks/antora-lunr.git
+    cd antora-lunr
+    git checkout plugin-377
+    npm install
+    cd ..
+fi
+
+# Build the unreleased generic SVG generator plugin
+if [ ! -d "extension" ]
+then
+    git clone https://gitlab.com/djencks/asciidoctor-generic-svg-extension.js.git extension
+    cd extension
+    git checkout issue-377-plugin
+    npm install
+    cd ..
+fi
+
+# Finally, put them all together to build the documentation site.
+cd ..
+DOCSEARCH_ENABLED=true DOCSEARCH_ENGINE=lunr doc/antora/node_modules/.bin/antora doc/netlify.yml

ba58f9e5cfc3ed9bc0a87b17fc0305f6807fe528
James Elliott <james@deepsymmetry.org>
2021-09-09 07:36:07 -0500

    Try to determine Netlify clojure version (debugging CI doc build)

--- doc/build.sh
+++ doc/build.sh
@@ -3,6 +3,9 @@
 # This script is run by Netlify continuous integration to build the
 # Antora site hosting the bytefield-svg user guide.
 
+# Test to try and figure out why CI is not working on Netlify
+clojure --version
+
 # If we haven't already, build bytefield-svg so we can create the
 # example diagrams.
 if [ ! -f "lib.js" ]

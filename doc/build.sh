713b93af4a2db03e86e9c7c9e54c9d0825bf3da5
James Elliott <james@deepsymmetry.org>
2021-09-09 07:46:37 -0500

    Another attempt to get Netlify doc build working.

--- doc/build.sh
+++ doc/build.sh
@@ -3,6 +3,10 @@
 # This script is run by Netlify continuous integration to build the
 # Antora site hosting the bytefield-svg user guide.
 
+# Try making Netlify realize I need clojure (--version gave an error, perhaps
+# that variant is supported only in a newer release?)
+clojure -version
+
 # If we haven't already, build bytefield-svg so we can create the
 # example diagrams.
 if [ ! -f "lib.js" ]

0e489533c82ff9764243745d355334946898f703
James Elliott <james@deepsymmetry.org>
2023-02-26 17:42:10 -0600

    Try publishing user guide from GitHub Action.

---
+++ .github/scripts/build_guide.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# There is no point in doing this if we lack the SSH key to publish the guide.
+if [ "$GUIDE_SSH_KEY" != "" ]; then
+
+    # If we haven't already, build bytefield-svg so we can create the
+    # example diagrams.
+    if [ ! -f "lib.js" ]
+    then
+        npm run release
+    fi
+
+    # Use it to build the documentation site.
+    npx antora --fetch doc/github-actions.yml
+
+    # Publish it to the right place on the Deep Symmetry web server.
+    rsync -avz doc/build/site/ guides@deepsymmetry.org:/var/www/guides/bytefield-svg/
+
+else
+    echo "No SSH key present, not building user guide."
+fi

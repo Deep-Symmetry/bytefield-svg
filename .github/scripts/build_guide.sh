df55538563546acb21142bae1ef978cb1bac2c95
James Elliott <james@deepsymmetry.org>
2023-03-04 17:44:02 -0600

    Add HTML link checking to user guide build.

--- .github/scripts/build_guide.sh
+++ .github/scripts/build_guide.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e  # Exit if any command fails.
+
 # There is no point in doing this if we lack the SSH key to publish the guide.
 if [ "$GUIDE_SSH_KEY" != "" ]; then
 
@@ -15,6 +17,10 @@ if [ "$GUIDE_SSH_KEY" != "" ]; then
 
     # Use it to build the documentation site.
     npx antora --fetch doc/github-actions.yml
+
+    # Make sure there are no broken links in the versions we care about.
+    curl https://htmltest.wjdp.uk | bash
+    bin/htmltest
 
     # Publish it to the right place on the Deep Symmetry web server.
     rsync -avz doc/build/site/ guides@deepsymmetry.org:/var/www/guides/bytefield-svg/
